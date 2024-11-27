0080a8e49637cbfb01668bd94e2c8b1d4e54ac08
Paulo Neto <114318023+pauloneto342@users.noreply.github.com>
2024-11-27 10:40:04 -0300

    Add  Monoclinic

--- DISPOSITIVOS-PROj.py
+++ DISPOSITIVOS-PROj.py
@@ -73,10 +73,35 @@ def plot_orthorhombic_p(a_length=1, b_length=2, c_length=3):
     ax.set_zlabel('Z')
     plt.show(block=False)
 
+def plot_monoclinic_p(a_length=1, b_length=2, c_length=3, beta=np.deg2rad(95)):
+    a = np.array([a_length, 0, 0])
+    b = np.array([b_length * np.cos(beta), b_length * np.sin(beta), 0])
+    c = np.array([0, 0, c_length])
+    points = []
+    for i in range(2):
+        for j in range(2):
+            for k in range(2):
+                points.append(i * a + j * b + k * c)
+    points = np.array(points)
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.scatter(points[:, 0], points[:, 1], points[:, 2], color='blue')
+    for p1 in points:
+        for p2 in points:
+            distance = np.linalg.norm(p1 - p2)
+            if np.isclose(distance, a_length) or np.isclose(distance, b_length) or np.isclose(distance, c_length):
+                ax.plot([p1[0], p2[0]], [p1[1], p2[1]], [p1[2], p2[2]], color='gray')
+    ax.set_title('Rede Monoclínica P')
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
+    plt.show(block=False)
+
 def main():
     plot_cubic_simple()
     plot_tetragonal_p()
     plot_orthorhombic_p()
+    plot_monoclinic_p()
     plt.show()
 
 if __name__ == "__main__":
